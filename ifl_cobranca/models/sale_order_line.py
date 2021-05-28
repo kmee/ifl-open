7f0aac7cfbff0d6539419bcc2b038734340f9ea8
Diego Paradeda <paradeda.diego@gmail.com>
2021-05-28 15:54:57 -0300

    [FIX] donation field name

--- ifl_cobranca/models/sale_order_line.py
+++ ifl_cobranca/models/sale_order_line.py
@@ -10,7 +10,7 @@ class SaleOrderLine(models.Model):
         result = self._prepare_br_fiscal_dict()
         if self.product_id and self.product_id.invoice_policy == 'delivery':
             result['fiscal_quantity'] = self.fiscal_qty_delivered
-        if self.product_id.is_donation:
+        if self.product_id.donation:
             result['fiscal_document_line_id'] = self.env.ref(
                 'l10n_br_fiscal.fiscal_document_line_dummy').id
         result.update(super()._prepare_invoice_line(qty))
