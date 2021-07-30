a602939b160b4499d6dd8482f261b1d5456fa5cb
Diego Paradeda <paradeda.diego@gmail.com>
2021-07-30 12:43:17 -0300

    [ADD] Google api and delivery carrier default data

---
+++ ifl_delivery_cost_api_data/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Ifl Default Google Maps delivery data',
+    'description': """Ifl Default Google Maps delivery data""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE',
+    'website': 'kmee.com.br',
+    'depends': [
+        'delivery_cost_api_maps',
+    ],
+    'data': [
+        'data/delivey_cost_api_data.xml'
+    ],
+    'demo': [
+    ],
+}
