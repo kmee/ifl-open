a64bfa90367bc47dee0e7bd2e1bdc97f2950ef5c
Diego Paradeda <paradeda.diego@gmail.com>
2021-09-02 17:56:32 -0300

    [ADD] Block NF-e validate if any product has no default_code

--- ifl_cobranca/models/account_invoice.py
+++ ifl_cobranca/models/account_invoice.py
@@ -1,6 +1,7 @@
 import copy
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
@@ -8,4 +9,11 @@ class AccountInvoice(models.Model):
 
     def enable_card_token(self):
         self.picking_ids.sale_id.enable_payment_token()
+
+    def action_invoice_open(self):
+        for line in self.invoice_line_ids:
+            if line.product_id and not line.product_id.default_code:
+                raise ValidationError(_('O produto %s não tem Referência Interna. Conserte-o antes de continuar.' % line.product_id.name))
+
+        return super(AccountInvoice, self).action_invoice_open()
 
