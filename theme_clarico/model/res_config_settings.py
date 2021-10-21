cd6c20d372691df15e6f56247c6c7a5e768ddb1c
Diego Paradeda <paradeda.diego@gmail.com>
2021-10-21 00:21:40 -0300

    [REF] refactor old python models

--- theme_clarico/model/res_config_settings.py
+++ theme_clarico/model/res_config_settings.py
@@ -6,8 +6,6 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-
-
     website_pricelist_display = fields.Many2one(
         comodel_name='product.pricelist',
         string='Website pricelist display',
