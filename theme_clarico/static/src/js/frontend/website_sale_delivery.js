a1424b667c6cae75ae8ce5177d9ca1fcc734ca9d
Diego Paradeda <paradeda.diego@gmail.com>
2021-10-21 16:07:12 -0300

    [ADD] hidden values without 35 percent

--- theme_clarico/static/src/js/frontend/website_sale_delivery.js
+++ theme_clarico/static/src/js/frontend/website_sale_delivery.js
@@ -41,14 +41,18 @@ odoo.define('theme_clarico.replace_strategy', function (require) {
         var values = {'contribution': contribution_input}
         dp.add(ajax.jsonRpc('/shop/update_strategy', 'call', values));
         // UPDATE FRONT END VALUES
-        var $contribution_price = $("span[name='contribution_price']");
         var $contribution_percent = $("span[name='contribution_percent']");
-
         $contribution_percent[0].innerText = contribution_input;
-        var $subtotal = $('#order_total_untaxed .oe_currency_value')
-        var subtotal = parseFloat($subtotal[0].innerText.replace(/\./g, '').replace(/,/g, '.'));
-        var real_price = Number((subtotal / 1.35).toFixed(2))
-        $contribution_price[0].innerText = real_price * contribution_input / 100;
+
+        // calc contribution
+        var $subtotal = $('#order_total_untaxed_hidden .sub_total_hidden')
+        var subtotal_real = parseFloat($subtotal[0].innerText);
+        var $contribution_price = $("span[name='contribution_price']");
+        $contribution_price[0].innerText = subtotal_real * contribution_input / 100;
+
+        //calc product price for each prod
+        //calc subtotal
+        //calc total
     }
 
     var $contribution_input = $("#custom_contribution input[name='contribution_input']");
