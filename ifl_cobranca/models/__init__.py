04f82d7b869d22a35da2335f54757ccc0d434160
Renan Hiroki Bastos <renan.hiroki@kmee.com.br>
2021-11-16 17:05:38 -0300

    [ADD] can_sell field to product.product and attribute.value.

--- ifl_cobranca/models/__init__.py
+++ ifl_cobranca/models/__init__.py
@@ -2,3 +2,4 @@ from . import ifl_cobranca
 from . import account_invoice
 from . import l10n_br_fiscal_document_line
 from . import sale_order_line
+from . import product_product
---
+++ ifl_cobranca/models/product_product.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models, _
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    can_sell = fields.Boolean()
+
+class ProductTemplateAttributeValue(models.Model):
+    """Inherit product template attribute value and make price extra
+    computed"""
+
+    _inherit = "product.template.attribute.value"
+
+    can_sell = fields.Boolean(
+        store=True,
+    )
