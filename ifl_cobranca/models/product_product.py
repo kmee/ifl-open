657dd645d46b748db822f58c01106d48ccb8b57c
Renan Hiroki Bastos <renan.hiroki@kmee.com.br>
2021-11-17 14:55:25 -0300

    [UPD] Refactor scheduled_set_can_sell method

--- ifl_cobranca/models/product_product.py
+++ ifl_cobranca/models/product_product.py
@@ -6,6 +6,19 @@ class ProductProduct(models.Model):
 
     can_sell = fields.Boolean(string="Disponível no site?")
 
+    @api.onchange('can_sell')
+    def set_is_published(self):
+        for record in self:
+            tmpl_id = record.product_tmpl_id
+            published_variants = self.env['product.template.attribute.value'].search([
+                ('product_tmpl_id', '=', tmpl_id.id),
+                ('can_sell', '=', True),
+            ])
+            if len(published_variants) == 0:
+                tmpl_id.is_published = False
+            else:
+                tmpl_id.is_published = True
+
     def scheduled_set_can_sell(self, enable=True):
         for record in self:
             tmpl_id = record.product_tmpl_id
@@ -16,15 +29,7 @@ class ProductProduct(models.Model):
                 ('product_attribute_value_id', 'in', record.attribute_value_ids.ids),
             ])
             product_template_attribute_value_ids.write({'can_sell': enable})
-            
-            published_variants = self.env['product.template.attribute.value'].search([
-                ('product_tmpl_id', '=', tmpl_id.id),
-                ('can_sell', '=', True),
-            ])
-            if len(published_variants) == 0:
-                tmpl_id.is_published = False
-            else:
-                tmpl_id.is_published = True
+            record.set_is_published()
 
 
 class ProductTemplateAttributeValue(models.Model):
