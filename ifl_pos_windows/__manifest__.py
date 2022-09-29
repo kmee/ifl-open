c6e14cf52a54559754f4e5a1b0236bd09f3d26b9
André Marcos Ferreira <andremarcosfe@gmail.com>
2022-09-29 12:14:08 -0300

    [ADD] IFL pos_windows

---
+++ ifl_pos_windows/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'IFL Windows POS',
+    'version': '12.0.0.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://kmee.com.br/',
+    'depends': [
+        'point_of_sale',
+        'l10n_br_pos'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ ifl_pos_windows/models/__init__.py
@@ -0,0 +1,1 @@
+from . import ifl_posconfig_inherit
