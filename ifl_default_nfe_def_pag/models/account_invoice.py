9ff66cd5e53da8a337d0b40fab850eb39930a5b7
Diego Paradeda <paradeda.diego@gmail.com>
2023-06-05 12:37:29 -0300

    [FIX] move detpag to doc confirm

--- ifl_default_nfe_def_pag/models/account_invoice.py
+++ ifl_default_nfe_def_pag/models/account_invoice.py
@@ -6,18 +6,22 @@ from odoo import api, fields, models
 class DocumentWorkflow(models.AbstractModel):
     _inherit = "l10n_br_fiscal.document.workflow"
 
-    def action_document_send(self):
-        pag_line = self.env['nfe.40.detpag'].search([
-            ('nfe40_detPag_pag_id', '=', self.id)
-        ])
-        if not pag_line:
-            invoice_id = self.env['account.invoice'].search([
-                ('fiscal_document_id', '=', self.id)
-            ])
-            line = self.env['nfe.40.detpag'].create({
-                'nfe40_indPag': '0',
-                'nfe40_tPag': '03',
-                'nfe40_vPag': invoice_id.amount_total,
-                'nfe40_detPag_pag_id': self.id,
-            })
-        return super().action_document_send()
+    def action_document_confirm(self):
+        for record in self:
+            pag_line = self.env["nfe.40.detpag"].search(
+                [("nfe40_detPag_pag_id", "=", record.id)]
+            )
+            if not pag_line:
+                invoice_id = self.env["account.invoice"].search(
+                    [("fiscal_document_id", "=", record.id)]
+                )
+                line = self.env["nfe.40.detpag"].create(
+                    {
+                        "nfe40_indPag": "0",
+                        "nfe40_tPag": "03",
+                        "nfe40_vPag": invoice_id.amount_total,
+                        "nfe40_detPag_pag_id": record.id,
+                    }
+                )
+
+        res = super().action_document_confirm()
